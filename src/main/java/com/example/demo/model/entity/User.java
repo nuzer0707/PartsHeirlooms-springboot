d08c30d9203c7a1755c8649a70f1c73a0f806b6b
ping <nuzer0707@gmail.com>
2025-05-29 00:43:50 +0800

    first

---
+++ src/main/java/com/example/demo/model/entity/User.java
@@ -0,0 +1,54 @@
+package com.example.demo.model.entity;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import com.example.demo.model.entity.enums.UserRole;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/*
+ * @Table(name = "users", uniqueConstraints = {
+ * 
+ * @UniqueConstraint(columnNames = "username"),
+ * 
+ * @UniqueConstraint(columnNames = "email") })
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name ="user_id")
+	private Integer userId;
+	
+	@Column(name = "username", nullable = false,length = 50)
+	private String username;
+	
+	@Column(name = "email",nullable = false,length = 255)
+	private String email;
+	
+	@Column(name = "password_hash",nullable = false,length = 255)
+	private String passwordHash;
+	
+	@Column(name = "hash_salt",nullable = false,length = 255)
+	private String hashSalt;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(name = "primary_role",nullable = false,columnDefinition = "Enum('BUYER','SELLER','ADMIN') DEFAULT 'BUYER'")
+	private UserRole primaryRole;
+	
+	@CreationTimestamp
+	@Column(name = "created_at", nullable = false,updatable = false)
+	private LocalDateTime createdAt;
+	
+	
+}
---
+++ src/main/java/com/example/demo/model/entity/enums/UserRole.java
@@ -0,0 +1,5 @@
+package com.example.demo.model.entity.enums;
+
+public enum UserRole {
+	BUYER, SELLER, ADMIN
+}
