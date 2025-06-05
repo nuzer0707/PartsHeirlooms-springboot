0ede7b5d99676618a2b24d9d7351d8f17d076210
ping <nuzer0707@gmail.com>
2025-06-06 00:16:21 +0800

    0606-h-2

---
+++ src/main/java/com/example/demo/exception/AccessDeniedException.java
@@ -0,0 +1,9 @@
+package com.example.demo.exception;
+
+public class AccessDeniedException extends RuntimeException{
+
+	public AccessDeniedException(String message) {
+		super(message);
+	}
+	
+}
---
+++ src/main/java/com/example/demo/exception/ProductOperationException.java
@@ -0,0 +1,8 @@
+package com.example.demo.exception;
+
+public class ProductOperationException extends RuntimeException{
+	public ProductOperationException(String message) {
+		super(message);
+	}
+
+}
